0b31e393ceeb166f15e7a27c68559b432930a627
v-xucxia <v-xucxia@PC-WINFFEE>
2016-10-04 11:28:30 +0800

    Add project files.

---
+++ AutoResizeItemsSample/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AutoResizeItemsSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Random rand = new Random();
+            List<Item> list = new List<Item>();
+            for (int i = 0; i < 10; i++)
+            {
+                int count = rand.Next(20);
+                String str = String.Empty;
+                for (int j = 0; j < count; j++)
+                {
+                    str += "word ";
+                }
+                var item = new Item
+                {
+                    Text = str
+                };
+                list.Add(item);
+            }
+            gridView.ItemsSource = list;
+            base.OnNavigatedTo(e);
+        }
+        
+        
+    }
+
+    public class Item
+    {
+        public String Text { get; set; }
+    }
+}
---
+++ AutoResizeItemsSample/OneRowPanel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AutoResizeItemsSample
+{
+    public class OneRowPanel:Panel
+    {
+        double itemHeight=0;
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            CoreApplication.Properties["GridViewSize"] = availableSize;
+            
+            int count = Children.Count;
+            foreach (FrameworkElement child in Children)
+            {
+                child.Measure(new Size(availableSize.Width/count,availableSize.Height));
+                if (child.DesiredSize.Height > itemHeight)
+                {
+                    itemHeight = child.DesiredSize.Height;
+                };
+            }
+            
+            // return the size available to the whole panel
+            return new Size(availableSize.Width, itemHeight);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            // Get the collection of children
+            UIElementCollection mychildren = Children;
+
+            // Get total number of children
+            int count = mychildren.Count;
+
+            // Arrange children
+            int i;
+            for (i = 0; i < count; i++)
+            {
+                //get the item Origin Point
+                Point cellOrigin = new Point(finalSize.Width / count * i,0);
+
+                // Arrange child
+                // Get desired height and width. This will not be larger than 100x100 as set in MeasureOverride.
+                double dw = mychildren[i].DesiredSize.Width;
+                double dh = mychildren[i].DesiredSize.Height;
+
+                mychildren[i].Arrange(new Rect(cellOrigin.X, cellOrigin.Y, dw, dh));
+
+            }
+            
+            // Return final size of the panel
+            return new Size(finalSize.Width, itemHeight);
+        }
+    }
+}
